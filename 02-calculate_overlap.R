8af725ddcbd38e63fddd3c15d8c544956e42078c
Trevor Day <trevor.k.day@gmail.com>
2026-06-24 15:19:30 -0400

    calculate dice by network for SM

--- 02-calculate_overlap.R
+++ 02-calculate_overlap.R
@@ -1,7 +1,8 @@
 library(tidyverse)
 library(ciftiTools)
 
-ciftiTools.setOption("wb_path", "/Applications/wb_view.app/Contents/usr/bin/wb_command")
+ciftiTools.setOption("wb_path", 
+"/Applications/wb_view.app/Contents/usr/bin/wb_command")
 
 setwd("~/MyDrive/Projects/parcel_overlap//")
 
@@ -78,7 +79,8 @@ expand <- comboGrid(results2$label, results2$label) %>%
     dice2 = results2$dice[match(Var2, results2$label)],
     avg_dice = (dice1 + dice2) / 2,
     avg_dice_Z = scale(avg_dice)[, 1]
-  )
+  ) %>%
+  arrange(desc(avg_dice))
 
 write_csv(expand, "all_combn_dice.csv")
 
@@ -86,4 +88,54 @@ ggplot(expand, aes(avg_dice)) +
   geom_density() +
   scale_x_continuous(limits = c(NA, 1)) +
   theme_bw()
- 
+
+# By network 
+
+networks <- read_csv("ColeAnticevic_parcels2networks.csv", show_col_types = FALSE) %>%
+  select(label2, L)
+
+networks2 <- left_join(results2, networks, by = join_by(label == label2)) %>%
+  mutate(
+    L = replace_values(L, "Visual2" ~ "Visual")
+  )
+
+dice_by_net <- networks2 %>%
+  group_by(L) %>%
+  summarize(
+    n = n(),
+    mean_dice = mean(dice),
+    sd_dice = sd(dice)
+  ) %>%
+  arrange(desc(mean_dice)) %>%
+  mutate(
+    se_dice = sd_dice / sqrt(n),
+    order = row_number(),
+    new_label = gsub("(?=[A-Z])", "\n", L, perl = TRUE) %>%
+      trimws()
+  )
+
+networks3 <- networks2 %>%
+  select(-order) %>%
+  left_join(
+    select(dice_by_net, L, order)
+  )
+
+ggplot(dice_by_net, aes(x = order, color = L)) +
+  geom_pointrange(aes(ymin = mean_dice - se_dice, y = mean_dice, 
+                      ymax = mean_dice + se_dice),
+                  size = 1, linewidth = 1.5) +
+  geom_jitter(
+    data = networks3,
+    aes(y = dice, fill = L),
+    alpha = 0.75, shape = 23, color = "black",
+    width = 0.15, height = 0
+  ) +
+  scale_x_continuous(breaks = 1:nrow(dice_by_net), 
+                     labels = dice_by_net$new_label) +
+  scale_y_continuous(limits = c(NA, 1)) +
+  coord_flip() +
+  theme_bw() +
+  labs(x = "Network", y = "Symmetry (Dice coef. [SE])") +
+  theme(legend.position = 'none')
+
+ggsave("plots/overlap_by_network.png", width = 5, height = 4)
